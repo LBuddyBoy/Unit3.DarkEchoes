c876d3776a689c938d813039ffd5abcb3dd9e784
Ethan Toups <ethantoups05@gmail.com>
2025-04-28 20:16:56 -0500

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,38 @@
+import { episodeList } from "./data";
+import { useState } from "react";
+
 export default function App() {
-  // TODO
+  console.log(episodeList);
+
+  const [selectedEpisode, setEpisode] = useState(null);
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <section id="episodesContainer">
+        <h2>Episodes</h2>
+        {episodeList.map((episode) => {
+          return (
+            <h3 className={selectedEpisode === episode ? "selectedEpisode" : ""} key={episode.id} onClick={() => setEpisode(episode)}>
+              {episode.title}
+            </h3>
+          );
+        })}
+      </section>
+
+      <section id="infoContainer">
+        {!selectedEpisode ? (
+          <h3>Click to view an episode!</h3>
+        ) : (
+          <>
+            <h3>{selectedEpisode.title}</h3>
+            <h4>Episode {selectedEpisode.id}</h4>
+            <h5>{selectedEpisode.description}</h5>
+          </>
+        )}
+      </section>
+    </>
+  );
 }
